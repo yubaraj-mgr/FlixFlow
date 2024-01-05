6c338f7d96594c5e431d0c057d4ebbc9ee4a7179
yubaraj-mgr <yubaraj.100mgr@gmail.com>
2024-01-05 11:17:19 +1100

    first commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ContactCard from "@/app/components/ContactCard";
+import styles from "./contact.module.css";
+import ContactForm from "@/app/components/ContactForm";
+
+const Contact = () => {
+    return (
+        <>
+            <div className={styles.container}>
+            <h1>Contact Us</h1>
+            <ContactCard />
+
+                <section className={styles.contact_section}>
+                    <h2>We'd love to hear <span> from you </span></h2>
+
+                    <ContactForm />
+                </section>
+            </div>
+
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d44606.21525143678!2d150.96189410476475!3d-33.80258846347384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzPCsDQ3JzM2LjAiUyAxNTDCsDU5JzQ1LjEiRQ!5e0!3m2!1sen!2sau!4v1704330451568!5m2!1sen!2sau"  referrerpolicy="no-referrer-when-downgrade"
+                width={100} height={450} style={{border: 0}} allowFullScreen="" loading="lazy" className={styles.mapping}
+                referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+        </>
+    );
+};
+
+export default Contact;
